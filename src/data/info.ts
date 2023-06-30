efea54c5a5367955d134d0f699f5df942e520763
Yash Raut <yash.raut@hnrtech.com>
2023-06-30 20:35:51 +0530

    changes

---
+++ src/data/info.ts
@@ -0,0 +1,130 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Yash Raut",
+  jobDescription: "Web Developer",
+  about: `Hello! I'm a Mumbai-based software engineer specializing in front-end development. With 3+ years of experience in the computer software industry, I've successfully completed numerous web projects from scratch. I have a passion for continuous learning and exploring new technologies to enhance my skills.
+  Additionally, I have a strong background in software development and 2+ years of experience in the IT industry. I excel in managing multiple projects simultaneously, consistently delivering exceptional results within tight deadlines.
+  In summary, I'm a dedicated software engineer with expertise in front-end development, backed by over 3 years of industry experience. I thrive on continuous learning and have a proven track record of successful project management. Let's collaborate and create something remarkable!
+  `,
+
+  experience: [
+    {
+      name: "Testlify",
+      position: "Frontend Lead",
+      location: "Mumbai, India",
+      startDate: "Aug 2022",
+      endDate: "Present",
+      description: [
+        "Testlify is the talent assessment platform helping companies hire the best talent quickly, easily, and accurately.",
+        "- Developing Talent Assessment Platform with Javascript- based Frameworks Vue and React for Frontend. ",
+        "- For Backend development working on Loopback."
+      ],
+    },
+    {
+      name: "Koyal Technologies LLP",
+      position: "Full Stack Developer",
+      location: "Mumbai, India",
+      startDate: "Apr 2021",
+      endDate: "Aug 2022",
+      description: [
+        "- Developed a Learning Management and Content Management System",
+        "- Used Vue Ecosystem (Nuxt , VueX) for frontend development",
+        "- Used Sails.js (Node.js) for backend development"
+      ],
+    },
+    {
+      name: "Blinsoft Technologies Pvt. Ltd",
+      position: "Flutter Mobile App Developer Intern",
+      location: "Remote",
+      startDate: "Aug 2020",
+      endDate: "Dec 2020",
+      description: [
+        "- Developed Hybrid(Android & iOS ) Mobile Application",
+        "- Beezhive (Canadian SuperApp). Used Flutter For building mobile app UI.",
+        "- Used Firebase (Baas platform) for backend services."
+      ],
+    },
+    {
+      name: " Bit Brothers Tech Pvt. Ltd",
+      position: "Flutter Mobile App Developer Intern",
+      location: "Mumbai, India",
+      startDate: "Nov 2019",
+      endDate: "Jan 2020",
+      description: [
+        "- Developed Hybrid(Android & iOS ) Mobile Application",
+        "- Gullymart a Grocery app. Used Flutter For building mobile app UI.",
+        "- Used Firebase (Baas platform) for backend services. Mobile App Developer Intern"
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Bachelors of Engineering in Information Technology",
+      position: "Vidyavardhini's College of Engineering and Technology ,Mumbai",
+      location: "University of Mumbai",
+      startDate: "2018",
+      endDate: "2021",
+      description: [
+        "CGPA : 8.5"
+      ],
+    },
+    {
+      name: "Diploma in Information Technology",
+      position: "Kala Vidyamandir Institute of Technology ,Mumbai",
+      location: "Maharashta State Board of Technical Education",
+      startDate: "2015",
+      endDate: "2018",
+      description: [
+        "Percentage : 82.53%"
+      ],
+    },
+    {
+      name: "High School",
+      position: "Aryan Education Society's High School",
+      location: "Secondary School Certificate ",
+      startDate: "2005",
+      endDate: "2015",
+      description: [
+        "Percentage : 83%"
+      ],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/yashraut362",
+    email: "yashraut362@gmail.com",
+    linkedin: "https://www.linkedin.com/in/yashraut362",
+  },
+
+  projects: [
+    {
+      title: "DCverse Movie Streaming App",
+      isFeatured: true,
+      thumbnail: "/assets/images/web/web1.jpg",
+      githubUrl: "https://github.com/yashraut362/DCVerse_MovieApp",
+      liveUrl: "https://dcverse.netlify.app/",
+    },
+    {
+      title: "Weshare File Sharing App",
+      isFeatured: true,
+      thumbnail: "/assets/images/web/web5.png",
+      githubUrl: "https://github.com/yashraut362/we-share",
+      liveUrl: "https://wesharefiles.vercel.app/",
+    },
+    {
+      title: "CryptoNation CryptoCurrency Tracker App",
+      isFeatured: true,
+      thumbnail: "/assets/images/web/web2.jpg",
+      githubUrl: "https://github.com/yashraut362/CryptoNation",
+      liveUrl: "http://cryptonation.vercel.app/",
+    },
+    {
+      title: "CoronaGo Covid-19 Tracker App",
+      isFeatured: true,
+      thumbnail: "/assets/images/web/web3.jpg",
+      githubUrl: "https://github.com/yashraut362/CoronaGo",
+      liveUrl: "https://coronagoo.netlify.app/",
+    },
+  ],
+};
